3057676b5edff0e217e1b183ca17a55f72c172ee
Taylor Harvet <larothion@aol.com>
2017-05-11 19:38:51 -0400

    Nav now works

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var methodOverride = require("method-override");
 var request = require("request");
 var PORT = process.env.PORT || 3000;
 var app = express();
+var path = require("path");
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(__dirname + "/public"));
 // Sets up the Express app to handle data parsing
@@ -27,6 +28,21 @@ db.sequelize.sync().then(function() {
  app.listen(PORT, function() {
    console.log("App Listening on Port: "+ PORT);
  });
+
+
+
+ // view engine setup
+app.set('views', path.join(__dirname, 'views'));
+//set up handlebars
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+//Configure public web folder
+app.use('/assets', express.static(path.join(__dirname, 'public', 'assets')));
 
 
 });
