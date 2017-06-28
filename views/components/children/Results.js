e5e2dbe9c36fb7b3489a7ccbd2c37905d09616c8
Taylor Harvet <larothion@aol.com>
2017-06-27 20:16:09 -0400

    version 1.0

--- views/components/children/Results.js
+++ views/components/children/Results.js
@@ -1,6 +1,125 @@
-// Include React
+import React from 'react';
+
+
+
+//start of component
+var Results = React.createClass ({
+   //objects of apps in the dashboard main panel
+    getInitialState : function() {
+     return {
+         displayList: [
+            {
+                displayName: "Hotform", 
+                email: "email@hotform.com",
+                followers: "80,000",
+                totalViews: "10,000,000",
+                game: "Hearthstone",
+                language: "en"
+            },
+            {
+              displayName: "Squillakilla", 
+              email: "email@squilla.com",
+              followers: "10,000",
+              totalViews: "20,000,000",
+              game: "Dark Souls",
+              language: "en"
+            }
+          ]
+      }
+    },
+
+      //stateless function for app container
+  var Info = (props) => {
+        return (
+
+        <div className="panel panel-default">
+          <div className="panelHeading">
+            <div className="row">
+              <div className="col-md-3">
+                <h3 className="panel-title text-center">Username </h3>
+              </div>
+              <div className="col-md-2">
+                <h3 className="panel-title text-center">Email</h3>
+              </div>
+              <div className="col-md-1">
+                <h3 className="panel-title text-center">Followers</h3>
+              </div>
+              <div className="col-md-2">
+                <h3 className="panel-title text-center">Total Views</h3>
+              </div>
+              <div className="col-md-2">
+                <h3 className="panel-title text-center">Game</h3>
+              </div>
+              <div className="col-md-2">
+                <h3 className="panel-title text-center">Language</h3>
+              </div>
+            </div>
+          </div>
+          <div className="panel-body">
+          <div> 
+                    <div className="row applicant-section">
+                        <div className="col-md-3 applicant-data">
+                          <h3 className="panel-title text-center">{props.displayName}</h3>
+                        </div>
+                        <div className="col-md-2 applicant-data">
+                          <h3 className="panel-title text-center">{props.email}</h3>
+                        </div>
+                        <div className="col-md-1 applicant-data">
+                          <h3 className="panel-title text-center">{props.followers}</h3>
+                        </div>
+                        <div className="col-md-2 applicant-data">
+                          <h3 className="panel-title text-center">{props.totalViews}</h3>
+                        </div>
+                        <div className="col-md-2 applicant-data">
+                          <h3 className="panel-title text-center">{props.game}</h3>
+                        </div>
+                        <div className="col-md-2">
+                          <h3 className="panel-title text-center">{props.language}</h3>
+                        </div> 
+                    </div> 
+                  <div>
+                    <button className="btn-lg btn-success app-buttons" > Accepted </button>
+                    <button className="btn-lg btn-danger app-buttons"> Declined </button>
+                  </div>
+            </div>
+        </div>
+        </div>
+
+      );
+},
+
+ //start of render function
+  render() {
+    var createRow = (element) => {
+      return <Info displayName={element.displayName}
+        email={element.email} followers={element.followers}
+        totalViews={element.totalViews} game={element.game} language={element.language}/>;
+    };
+
+   return (
+        <div>
+              {this.state.getInitialState.
+                displayList.map(createRow)}
+
+       </div>
+    )
+  },
+
+});
+
+
+
+//exporting Software component
+module.exports = Results;
+
+
+
+
+
+
+/*// Include React
 var React = require("react");
-var AddInfluencer = require("AddInfluencer");
+var AddInfluencer = require("./AddInfluencer");
 
 // Creating the Results component
 var Results = React.createClass({
@@ -20,14 +139,22 @@ var Results = React.createClass({
 
   newApplicant: function() {
       this.setState ({
-        displayName: ['Hotform',"PyroElementalist"],
-        email: ["email@email.com"],
-        followers: ["80,000"],
-        totalViews: ["10,000,000"],
-        game: ["Hearthstone"],
-        language: ["en"]
-      })
-
+        applicantInfo: [
+          {
+            displayName: ['Hotform',"PyroElementalist"]
+          }, {
+            email: ["email@hotform.com","Email@pyro.com"]
+          }, {
+            followers: ["80,000", "65,000"]
+          }, {
+           totalViews: ["10,000,000", "11,000,000"]
+          }, {
+            game: ["Hearthstone", "Dark Souls"]
+          }, {
+            language: ["en", "finland"]
+          }
+        ]
+      });
   },
  
   appendApplicant: function appendApplicant(data) {
@@ -49,7 +176,7 @@ var Results = React.createClass({
                 <div> 
                   <div className="row applicant-section">
                       <div className="col-md-3 applicant-data">
-                        <h3 className="panel-title text-center">{this.state.displayName}</h3>
+                        <h3 className="panel-title text-center">{this.state.applicantInfo.displayName}</h3>
                       </div>
                       <div className="col-md-2 applicant-data">
                         <h3 className="panel-title text-center">{this.state.email}</h3>
@@ -71,47 +198,15 @@ var Results = React.createClass({
                   <button className="btn-lg btn-danger app-buttons"> Declined </button>
                 </div>
             )
-        });
     },
 
-  render: function() {
-    return (
-      <div className="panel panel-default">
-        <div className="panelHeading">
-          <div className="row">
-            <div className="col-md-3">
-              <h3 className="panel-title text-center">Username </h3>
-            </div>
-            <div className="col-md-2">
-              <h3 className="panel-title text-center">Email</h3>
-            </div>
-            <div className="col-md-1">
-              <h3 className="panel-title text-center">Followers</h3>
-            </div>
-            <div className="col-md-2">
-              <h3 className="panel-title text-center">Total Views</h3>
-            </div>
-            <div className="col-md-2">
-              <h3 className="panel-title text-center">Game</h3>
-            </div>
-            <div className="col-md-2">
-              <h3 className="panel-title text-center">Language</h3>
-            </div>
-          </div>
-        </div>
-        <div className="panel-body">
-          <button className="mount" onClick={this.renderApplicant}> Mount!! </button>
-          
-        </div>
-      </div>
-    );
-  }
+  
 });
 
 //Export the component back for use in other files
 module.exports = Results;
 
-
+*/
 //on form submit we need the form to send us all the info in the form.
 // We then need to do a Twitch API call and store that information in a database
 // Then the front-end should display the Twitch_url, Email, followers, views, game, and language of the streamer
